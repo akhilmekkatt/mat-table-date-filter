abd0beaeff2376d35e005ce01794674f91c9182e
akhilmekkatt <akhilmekkatt@gmail.com>
2022-11-21 14:13:11 +0530

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { TableComponent } from './table/table.component';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatTableModule],
   declarations: [AppComponent, TableComponent],
   bootstrap: [AppComponent],
 })
